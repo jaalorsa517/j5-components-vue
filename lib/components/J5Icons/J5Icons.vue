996d23d2e68b5097767dd48105473509f4fd0252
Jaime Ortiz <jaalorsa517@hotmail.com>
2023-09-11 23:33:13 -0500

    Agregando j5-icons

--- lib/components/J5Icons/J5Icons.vue
+++ lib/components/J5Icons/J5Icons.vue
@@ -2,31 +2,40 @@
 import { inject } from "vue";
 import { dic } from "./icons";
 
-const dicInyect = inject("j5-icons") || {}
+const dicInyect = inject<Record<string, string>>("j5-icons", {})
 
 const dicDefinitve: Record<string, string> = { ...dic, ...dicInyect }
 const { name, color } = defineProps({ name: String, color: { type: String, default: "currentColor" } });
 
 const getIco = (name: string) => {
+  const ico = dicDefinitve[name];
+  if (!ico)
+    return "?";
+
   let tagName = "path";
   let colorProp = "fill";
-  let strokeWidth = "";
-  const ico = dicDefinitve[name];
-  if (ico) {
-    if (ico.includes("<polygon")) {
-      tagName = "polygon";
-    } else if (ico.includes("<rect")) {
-      tagName = "rect";
-    } else if (ico.includes("<circle")) {
-      tagName = "circle";
-      colorProp = "stroke";
-      strokeWidth = 'stroke-width="1"';
-    }
-    let re = new RegExp(`<${tagName}`, "g");
-    return ico.replace(re, `<${tagName} ${colorProp}="${color}" ${strokeWidth}`);
-  } else {
-    return "?";
+  let ohterAttrs = "";
+
+  if (Boolean(ico.match(/<path(.*)stroke="(.*)">/g)?.length)) {
+    ohterAttrs = "fill=none";
+    colorProp = "stroke";
+  } else if (Boolean(ico.match(/<polygon(.*)stroke="(.*)">/g)?.length)) {
+    tagName = "polygon";
+    ohterAttrs = "fill=none";
+    colorProp = "stroke";
+  } else if (ico.includes("<polygon")) {
+    tagName = "polygon";
+  } else if (ico.includes("<rect")) {
+    tagName = "rect";
+  } else if (ico.includes("<circle")) {
+    tagName = "circle";
+    colorProp = "stroke";
+  } else if (ico.includes("<line")) {
+    tagName = "line";
+    colorProp = "stroke";
   }
+  const re = new RegExp(`<${tagName}`, "g");
+  return ico.replace(re, `<${tagName} ${colorProp}="${color}" ${ohterAttrs}`);
 };
 </script>
 <template>
