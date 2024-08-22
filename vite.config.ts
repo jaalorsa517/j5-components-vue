04adc504da2046b778a524e30137969e395085c1
jaalorsa517 <jaalorsa517@hotmail.com>
2024-08-21 23:34:10 -0500

    Configurando el proyecto

--- vite.config.ts
+++ vite.config.ts
@@ -18,16 +18,14 @@ export default defineConfig({
       external: ["vue"],
       output: {
         globals: {
-          vue: "Vue",
+          vue: "Vue"
         },
       },
     },
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "lib"),
       lib: path.resolve(__dirname, "lib"),
-      test: path.resolve(__dirname, "test"),
       styles: path.resolve(__dirname, "lib/styles"),
     },
   },
@@ -45,15 +43,20 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       all: true,
-      enabled: true,  
-      exclude: ["src/**", "test/**", "lib/shared/types/**", "components-vue/**"],
+      enabled: true,
+      exclude: [
+        "src/**",
+        "test/**",
+        "lib/shared/types/**",
+        "components-vue/**",
+      ],
       reporter: ["json-summary", "html"],
       thresholds: {
         statements: 100,
         branches: 100,
         functions: 100,
         lines: 100,
-      }
+      },
     },
   },
 });
